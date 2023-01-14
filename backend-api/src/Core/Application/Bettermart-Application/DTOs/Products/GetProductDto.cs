3ace1131334a683379520afb7aa4c0b35da64681
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-01-14 11:51:57 +0530

    feat: add description field to product ✨

--- backend-api/src/Core/Application/Bettermart-Application/DTOs/Products/GetProductDto.cs
+++ backend-api/src/Core/Application/Bettermart-Application/DTOs/Products/GetProductDto.cs
@@ -8,7 +8,8 @@ namespace Bettermart_Application.DTOs.Products
         public string? Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty; 
         public string Brand { get; set; } = string.Empty;  
-        public string Category { get; set; } = string.Empty;     
+        public string Category { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public string[] Image { get; set; } = Array.Empty<string>();
         public float Price { get; set; }
         public int StockCount { get; set; }
