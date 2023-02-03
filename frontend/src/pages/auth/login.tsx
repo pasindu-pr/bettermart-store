2bc02be05221fb0eb3a1ddaae67cac40ff11376d
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2023-02-03 14:27:48 +0530

    refactor: add layout for auth pages ♻️

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
-import { Button, H2, Input, Image, Error } from "../../components";
+import { Button, Input, Error } from "../../components";
+import { AuthPageLayout } from "../../layouts";
 import { AuthService } from "../../services";
 import { LoginInputs } from "../../types/auth/inputs";
 
@@ -46,65 +47,49 @@ export default function Login() {
 
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <div className="w-full flex justify-center">
-              <div className="relative w-12 h-12">
-                <Image
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt="Bettermart Logo"
-                />
-              </div>
-            </div>
+      <AuthPageLayout title="Sign in to your account">
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <Input
+              {...register("email", {
+                required: "Email is required",
+              })}
+              type="email"
+              placeholder="Email"
+              additionalStyles="rounded-t"
+            />
 
-            <H2 text="Sign in to your account" />
+            <Input
+              {...register("password", {
+                required: "Password is required",
+              })}
+              type="password"
+              placeholder="Password"
+              additionalStyles="rounded-b"
+            />
           </div>
-          <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <Input
-                {...register("email", {
-                  required: "Email is required",
-                })}
-                type="email"
-                placeholder="Email"
-                additionalStyles="rounded-t"
-              />
 
-              <Input
-                {...register("password", {
-                  required: "Password is required",
-                })}
-                type="password"
-                placeholder="Password"
-                additionalStyles="rounded-b"
-              />
+          <div className="mt-2">
+            {errors.email && <Error message={errors.email.message as string} />}
+
+            {errors.password && (
+              <Error message={errors.password.message as string} />
+            )}
+          </div>
+
+          <Button title="Login" size="small" />
+
+          <div className="flex w-full justify-center gap-4">
+            <div
+              onClick={handleGoogleLogin}
+              className="flex items-center shadow-md px-2 cursor-pointer border-2 border-indigo-50 gap-3 rounded-3xl py-2"
+            >
+              <img src="/images/social/google.svg" className="w-6" />
             </div>
-
-            <div className="mt-2">
-              {errors.email && (
-                <Error message={errors.email.message as string} />
-              )}
-
-              {errors.password && (
-                <Error message={errors.password.message as string} />
-              )}
-            </div>
-
-            <Button title="Login" size="small" />
-
-            <div className="flex w-full justify-center gap-4">
-              <div
-                onClick={handleGoogleLogin}
-                className="flex items-center shadow-md px-2 cursor-pointer border-2 border-indigo-50 gap-3 rounded-3xl py-2"
-              >
-                <img src="/images/social/google.svg" className="w-6" />
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+          </div>
+        </form>
+      </AuthPageLayout>
     </>
   );
 }
--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -7,6 +7,7 @@ import { AuthService } from "../../services";
 import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 import { RegisterInputs } from "../../types/auth/inputs";
+import { AuthPageLayout } from "../../layouts";
 
 const Register = () => {
   const router = useRouter();
@@ -37,81 +38,65 @@ const Register = () => {
 
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <div className="w-full flex justify-center">
-              <div className="relative w-12 h-12">
-                <Image
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt="Bettermart Logo"
-                />
-              </div>
-            </div>
+      <AuthPageLayout title="Create your account">
+        <form
+          className="mt-8 space-y-6"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+          <div className="rounded-md shadow-sm -space-y-px">
+            <Input
+              type="email"
+              placeholder="Email"
+              additionalStyles="rounded-t"
+              {...register("email", { required: "Email is required" })}
+            />
 
-            <H2 text="Create your account" />
+            <Input
+              type="text"
+              placeholder="Name"
+              additionalStyles="rounded-0"
+              {...register("fullName", { required: "Full name is required" })}
+            />
+
+            <Input
+              type="password"
+              placeholder="Password"
+              additionalStyles="rounded-b"
+              {...register("password", {
+                required: true,
+                minLength: {
+                  value: 6,
+                  message: "Password must be atleast 6 characters long",
+                },
+              })}
+            />
           </div>
-          <form
-            className="mt-8 space-y-6"
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-          >
-            <div className="rounded-md shadow-sm -space-y-px">
-              <Input
-                type="email"
-                placeholder="Email"
-                additionalStyles="rounded-t"
-                {...register("email", { required: "Email is required" })}
-              />
 
-              <Input
-                type="text"
-                placeholder="Name"
-                additionalStyles="rounded-0"
-                {...register("fullName", { required: "Full name is required" })}
-              />
+          <div className="mt-2">
+            {errors.fullName && (
+              <Error message={errors.fullName.message as string} />
+            )}
+            {errors.email && <Error message={errors.email.message as string} />}
+            {errors.password && (
+              <Error message={errors.password.message as string} />
+            )}
+          </div>
 
-              <Input
-                type="password"
-                placeholder="Password"
-                additionalStyles="rounded-b"
-                {...register("password", {
-                  required: true,
-                  minLength: {
-                    value: 6,
-                    message: "Password must be atleast 6 characters long",
-                  },
-                })}
-              />
+          <Button title="Login" size="small" type="submit" />
+
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              <Link
+                href="/auth/login"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Already have an account? Login from here
+              </Link>
             </div>
-
-            <div className="mt-2">
-              {errors.fullName && (
-                <Error message={errors.fullName.message as string} />
-              )}
-              {errors.email && (
-                <Error message={errors.email.message as string} />
-              )}
-              {errors.password && (
-                <Error message={errors.password.message as string} />
-              )}
-            </div>
-
-            <Button title="Login" size="small" type="submit" />
-
-            <div className="flex items-center justify-between">
-              <div className="text-sm">
-                <Link
-                  href="/auth/login"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Already have an account? Login from here
-                </Link>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+          </div>
+        </form>
+      </AuthPageLayout>
     </>
   );
 };
